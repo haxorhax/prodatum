59acc21542daffa38c7c5cbcb2865e9614f41416
haxorhax <haxorhax@haxorhax.com>
2022-08-28 09:50:37 -0700

    Round lucky seven

--- src/prodatum.cpp
+++ src/prodatum.cpp
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 				" -d id    \tdevice to open (default: previous)\n"
 				" -a       \tdisable autoconnect\n"
 				" -i       \tstart minimized\n"
-				" -ti title\twindow title\n", PROJECT_VER);
+				" -ti title\twindow title\n", PROJECT_VERSION);
 		return 1;
 	}
 	// loading... please wait.
@@ -604,7 +604,7 @@ void PD_UI::create_about()
 	OS = "GNU/Linux";
 #endif
 	char buf[512];
-	snprintf(buf, 512, "prodatum %s\nfor %s", PROJECT_VER, OS);
+	snprintf(buf, 512, "prodatum %s\nfor %s", PROJECT_VERSION, OS);
 	about_text->copy_label(buf);
 }
 
